cabe2e4c013f3c327f6454838234812c6c34acdc
isanch14 <isanch14@stu.southtexascollege.edu>
2020-05-02 20:30:39 -0500

    Main Layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+    <v-app>
+    <v-app-bar app clipped-left dark color="primary">
+      <v-toolbar-title>The Pong Game</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer app clipped class="purple ligthten-1"> </v-navigation-drawer>
+    <v-content class="error noOverflow">
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
